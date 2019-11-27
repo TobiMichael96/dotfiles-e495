e6231a60f5ab5d4a6c1fd033c3247a25256fb713
Tobias <tobiasmichael@mail.de>
2019-11-27 01:55:31 +0100

    Automatic update in submodule at Wed 27 Nov 2019 01:55:31 AM CET.

--- screenlayout/checkscreens.sh
+++ screenlayout/checkscreens.sh
@@ -1,18 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 connected=$(cat /sys/class/drm/card0/card0-HDMI-A-1/status)
 if [ "$connected" == "connected" ]; then
-    	sh ~/.screenlayout/withTV.sh
-    	sleep 1
-    	bspc monitor LVDS1 -d 1 2 3 4 5 6 7 8
-    	bspc monitor HDMI1 -d 9 10
+    	sh /home/tobias/.screenlayout/withTV.sh
+	#sed -i 's/HandleLidSwitch=.*/HandleLidSwitch=ignored/' /etc/systemd/logind.conf
+        #sed -i 's/HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=ignored/' /etc/systemd/logind.conf
+	bspc monitor LVDS1 -d 1 2 3 4 5 6 7 8
+	bspc monitor HDMI1 -d 9 10
     	notify-send "Screen" "Second screen added..."
 else
-    	sh ~/.screenlayout/withoutTV.sh
-    	sleep 1
-    	bspc monitor LVDS1 -d 1 2 3 4 5 6 7 8 9 10
-#    	bspc monitor HDMI1 -d 9 10
+    	sh /home/tobias/.screenlayout/withoutTV.sh
+	#sed -i 's/HandleLidSwitch=.*/HandleLidSwitch=suspend/' /etc/systemd/logind.conf
+        #sed -i 's/HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=suspend/' /etc/systemd/logind.conf
 	notify-send "Screen" "Second screen removed..."
+	bspc monitor LVDS1 -d 1 2 3 4 5 6 7 8
 fi
 
-sh ~/dotfiles/scripts/hdmi_sound_toggle.sh
-wal -i ~/dotfiles/background
+sh /home/tobias/dotfiles/scripts/hdmi_sound_toggle.sh
+wal -i /home/tobias/dotfiles/background
