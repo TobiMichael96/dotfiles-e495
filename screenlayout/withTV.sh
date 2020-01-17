5f31acf947e7dc536e8bc301aba97bd5f5465d0e
Tobias Effner <me@tobiasmichael.de>
2020-01-18 00:54:22 +0100

    Automatic update in submodule at Sat 18 Jan 2020 12:54:22 AM CET.

--- screenlayout/withTV.sh
+++ screenlayout/withTV.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output LVDS1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
