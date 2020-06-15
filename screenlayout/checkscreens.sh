ca892dff7c4784c07a95a1bc8ce6f00023dd57d4
Tobias Effner <me@tobiasmichael.de>
2020-06-15 16:22:16 +0200

    Automatic update in submodule at Mon 15 Jun 2020 04:22:16 PM CEST.

--- screenlayout/checkscreens.sh
+++ screenlayout/checkscreens.sh
@@ -4,15 +4,15 @@ if [ "$connected" == "connected" ]; then
     	sh /home/tobias/.screenlayout/withTV.sh
 	#sed -i 's/HandleLidSwitch=.*/HandleLidSwitch=ignored/' /etc/systemd/logind.conf
         #sed -i 's/HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=ignored/' /etc/systemd/logind.conf
-	bspc monitor eDP -d 1 2 3 4 5 6 7 8
-	bspc monitor HDMI-A-0 -d 9 10
+	bspc monitor eDP -d 1 2 3 4 5 6 7 8 9 10 11 12
+	bspc monitor HDMI-A-0 -d 13 14
     	notify-send "Screen" "Second screen added..."
 else
     	sh /home/tobias/.screenlayout/withoutTV.sh
 	#sed -i 's/HandleLidSwitch=.*/HandleLidSwitch=suspend/' /etc/systemd/logind.conf
         #sed -i 's/HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=suspend/' /etc/systemd/logind.conf
 	notify-send "Screen" "Second screen removed..."
-	bspc monitor eDP -d 1 2 3 4 5 6 7 8
+	bspc monitor eDP -d 1 2 3 4 5 6 7 8 9 10 11 12
 fi
 
 wal -i /home/tobias/dotfiles/background
